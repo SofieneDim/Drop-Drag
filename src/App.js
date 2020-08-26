1826eb13605fee898004ace931072230fdd8ab91
sofienedim <dimessisofiene@gmail.com>
2020-08-26 14:26:27 +0100

    auto push

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 
-import DraggableArea from "./components/DraggableArea";
+// import DraggableArea from "./components/DraggableArea";
 import rootReducer from './store/reducers';
 import logger from './store/middlewares/logger';
+import Testy from './testy';
 
 const composeEnheancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -16,7 +17,8 @@ class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <DraggableArea />
+                {/* <DraggableArea /> */}
+                <Testy />
             </Provider>
         );
     };
