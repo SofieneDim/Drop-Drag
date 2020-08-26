c6213cc25bbee8eb6034f17d4c013945c8f85330
MehdyDim <mehdydimassi1@gmail.com>
2020-08-26 18:06:10 +0100

    before link mode

--- src/testy.js
+++ src/testy.js
@@ -8,9 +8,8 @@ class Testy extends Component {
         return (
             <div>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-                <div id="div1" style="width: 100px; height: 100px; top:0; left:0; background:#e53935 ; position:absolute;"></div>
-                <div id="div2" style="width: 100px; height: 100px; top:0; left:300px; background:#4527a0 ; position:absolute;"></div>
+                <div id="div1" style={{width: "100px", height: "100px", top:"0", left:"0", background:"#e53935", position:"absolute"}}></div>
+                <div id="div2" style={{width: "100px", height: "100px", top:"0", left:"300px", background:"#4527a0" , position:"absolute"}}></div>
                 <svg width="500" height="500"><line x1="50" y1="50" x2="350" y2="50" stroke="red" /></svg>
             </div>
         );
