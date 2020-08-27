3c06fd0fb17487b5548856e5c0057c5234b53250
sofienedim <dimessisofiene@gmail.com>
2020-08-27 12:19:00 +0100

    arrow fixed done

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,6 @@ class App extends React.Component {
         arrowsIds: [],
     };
 
-
-    componentDidMount = async prevProps => {
-        const ctx = document.getElementById("c").getContext("2d");
-        this.displayArrows(ctx, 900, 500, 100, 100);
-    };
-
     displayArrows = (ctx, fromx, fromy, tox, toy) => {
         ctx.beginPath();
         canvas_arrow(ctx, fromx, fromy, tox, toy);
@@ -42,25 +36,20 @@ class App extends React.Component {
         ctx.beginPath();
         const domTask_1 = document.getElementById(idFrom);
         const domTask_2 = document.getElementById(idTo);
-
-
-
         const fromx = domTask_1.getBoundingClientRect().x + 30;
         const fromy = domTask_1.getBoundingClientRect().y + 30;
-
         const tox = domTask_2.getBoundingClientRect().x + 15;
         const toy = domTask_2.getBoundingClientRect().y + 30;
-
         this.displayArrows(ctx, fromx, fromy, tox, toy);
     };
 
     render() {
 
         const arrows = this.state.arrowsIds.map(arrowId =>
-            <canvas id={arrowId} key={arrowId} width="500" height="500" style={{ position: "absolute" }} />
+            <canvas id={arrowId} key={arrowId} width="1000" height="500" style={{ position: "absolute" }} />
         );
         return ([
-            <canvas id={"c"} key={5} width="500" height="500" style={{ position: "absolute" }} />,
+            // <canvas id={"c"} key="1" width="1000" height="500" style={{ position: "absolute" }} />,
             ...arrows,
             <DraggableArea
                 key="2"
